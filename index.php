060cc92dfe2ef84e0f1eaf615eb52341ac7970ef
Ashish Kumar <ashish.kumar@payu.in>
2020-09-01 22:17:28 +0530

    added Sample App code to repo

---
+++ index.php
@@ -0,0 +1,155 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>PayU</title>
+<meta name="description" content="">
+<link rel="stylesheet" type="text/css" href="css/layout.css">
+<link rel="stylesheet" type="text/css" href="css/typography.css">
+</head>
+<body>
+<div class="main">
+<header>
+<div class="header-main">
+<div class="hd-logo"><img src="images/logo.svg" alt="PayU Logo"></div>
+<div class="hd-nav">
+<ul>
+<li><a href="https://github.com/payu-india/payu-sdk-php"><i><img src="images/github-icon.svg"></i>View on Github</a></li>
+</ul>
+</div>
+</div>
+</header>
+<section>
+<div class="banner-main">
+<div class="bl-cn">
+<div class="container">
+<div class="bn-crt-img"><img src="images/banner-cart.svg"></div>
+<div class="bn-crt-text">
+<h1>Payments Demo</h1>
+<span class="yellow-bx">Test Mode</span>
+</div>
+</div>
+
+<div class="form-main">
+
+<div class="form-white-box">
+<div class="form-hd">Complete Or<span>Generate</span>your test payment details below</div>
+<form action="https://payu-checkout.herokuapp.com/" method="post">
+<div class="frm-bx-content">
+<div class="form-row">
+<label>Name</label>
+<div class="form-inp">
+<input type="text" name="firstname" value = "" id = "input_name" placeholder="Nita Nisha"  value="" >
+</div>
+</div>
+<div class="form-row">
+<label>Delivery Address</label>
+<div class="form-inp">
+<input type="text" name="address" placeholder="90, AnshuGarh, Hisar - 193389" >
+</div>
+</div>
+<div class="form-row">
+<label>Contact</label>
+<div class="form-inp">
+<input type="text" name="phonenumber" placeholder="Enter your phone number">
+</div>
+</div>
+
+
+ <input type="submit" class="cmn-btn" value="                                                            Pay ₹ 10.00                                                              " >
+</form>
+
+</div>
+</div>
+<div class="security-logo">
+<ul>
+<li><img src="images/norton-secure.svg"></li>
+<li><img src="images/verified-visa.svg"></li>
+<li><img src="images/mastercard.svg"></li>
+<li><img src="images/safekey.svg"></li>
+<li><img src="images/pcidss.svg"></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section>
+<div class="common-container">
+<div class="product-sm">
+<div class="container">
+<h2>Order Summary</h2>
+<div class="product-detail">
+<ul>
+<li>
+<div class="prd-typ-main">
+<div class="prd-img"><img src="images/tshirt.png"></div>
+<p>
+<span class="prd-nm">T-shirt</span>
+<span class="prd-typ">medium-size</span>
+</p>
+</div>
+<div class="prd-price">2 x ₹ 1.00 <span>₹ 2.00</span></div>
+</li>
+<li>
+<div class="prd-typ-main">
+<div class="prd-img"><img src="images/Keyboard_image.jpg"></div>
+<p>
+<span class="prd-nm">Keyboard</span>
+<span class="prd-typ">Standard-size</span>
+</p>
+</div>
+<div class="prd-price">2 x ₹ 1.00 <span>₹ 2.00</span></div>
+</li>
+<li>
+<div class="prd-typ-main">
+<div class="prd-img"><img src="images/Coffee_mug.jpg"></div>
+<p>
+<span class="prd-nm">Coffee Mug</span>
+<span class="prd-typ">Standard-size</span>
+</p>
+</div>
+<div class="prd-price">2 x ₹ 3.00 <span>₹ 6.00</span></div>
+</li>
+
+</ul>
+
+<ul class="subtotal-main">
+<li class="custom-con-main">
+<div class="prd-typ-main custom-con">
+<p>
+<span class="prd-nm">SUBTOTAL</span>
+</p>
+</div>
+<div class="prd-price"><span>₹ 10.00</span></div>
+</li>
+<li class="custom-con-main">
+<div class="prd-typ-main custom-con">
+<p>
+<span class="prd-nm">SHIPPING</span>
+</p>
+</div>
+<div class="prd-price"><span>Free</span></div>
+</li>
+<li class="pr-tol-amt">
+<div class="prd-typ-main custom-con">
+<p>
+<span class="prd-nm">Total Amount To Pay</span>
+</p>
+</div>
+<div class="prd-price"><span>₹ 10.<small>00</small></span></div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</section>
+</div>
+
+
+  </body>
+</html>
---
+++ payu/tests/unit_test.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionTest extends TestCase
+{
+  public function testAddReturnsTheCorrectSum()
+  {
+    require 'functions.php';
+
+    $this->assertEquals(4,add(2,2));
+    $this->assertEquals(8,add(5,3));
+
+  }
+}
+
+
+
+ ?>
